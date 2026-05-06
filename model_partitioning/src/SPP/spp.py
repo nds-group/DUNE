17adbc335107f410b95f4af28370176984bb2962
ddeandres <david.deandres@imdea.org>
2026-05-06 12:40:08 +0200

    added cardinality constraint, k, to the SPP

--- model_partitioning/src/SPP/spp.py
+++ model_partitioning/src/SPP/spp.py
@@ -85,7 +85,7 @@ def get_block_gain(block, importance_matrix, score_vector):
     return cost, feats
 
 
-def solve_SPP_with_ILP(spp, obj_function, minimize=True, save=False):
+def solve_SPP_with_ILP(spp, obj_function, minimize=True, max_parts=None, save=False):
     """Finds a solution to the SPP using an Integer Linear Programming (ILP) approach.
     The objective function must be linear.
     Parameters
@@ -130,8 +130,12 @@ def solve_SPP_with_ILP(spp, obj_function, minimize=True, save=False):
     part_model = pulp.LpProblem('Part_Model', objective)
     part_model += pulp.lpSum((cost_values[i] * x[part] for i, part in enumerate(possible_partitions)))
 
+    if max_parts is not None:
+        max_parts = min(max_parts, spp.n_classes - 1)
+    else:
+        max_parts = spp.n_classes - 1
     part_model += (
-        pulp.lpSum([x[part] for part in possible_partitions]) <= spp.n_classes - 1,
+        pulp.lpSum([x[part] for part in possible_partitions]) <= max_parts,
         "Maximum_number_of_partitions",
     )
 
@@ -186,8 +190,8 @@ class SPP:
     classes_list = None
 
     def __init__(self, n_classes, n_features, unwanted_classes, use_case, weights_file=None, classes_subset=None,
-                 fix_level=None, weights_df=None, f1_file=None, f1_df=None):
-        self.fix_level = fix_level
+                 k=None, weights_df=None, f1_file=None, f1_df=None):
+        self.k = k
         self.log = logging.getLogger(self.__class__.__name__)
         self.use_case_name = use_case
         self.n_classes = n_classes
@@ -195,6 +199,7 @@ class SPP:
         if weights_df is None:
             if weights_file is None:
                 raise ValueError('Either weights_df or weights_file must be provided')
+            self.log.info('Loading weights from %s', weights_file)
             self.weights_df = pd.read_csv(weights_file)
         else:
             self.weights_df = weights_df
@@ -267,20 +272,16 @@ class SPP:
         return cluster_sol
 
     def solve_spp_greedy(self, gain_function=get_block_gain, show_plot_gain=True, save=False, print_console=False):
-        # it is important that the current_blocks_set are a set, since we will be adding and removing elements
-        current_blocks_set = set(range(1, self.n_classes+1)) # the worst case scenario is having a class per cluster
+        current_blocks_set = set(range(1, self.n_classes+1))
 
-        # Compute the gains for the initial blocks
         gain_map = {}
         blocks_onehot = [onehot(block_id, self.n_classes) for block_id in current_blocks_set]
         for key, key_onehot in zip(current_blocks_set, blocks_onehot):
             gain_map[key] = gain_function(np.array(key_onehot), self.feature_importance, self.f1_scores)
 
-        # Algorithm 1: SPP greedy algorithm
-        dendogram = {} # ToDo: rename dendogram dictionary, as it is not a really a dendogram
-        gains_list = [] # we cache gains to avoid re-computing them at each level
-        for i in range(self.n_classes - 1):
-            # generator with all pairs
+        dendogram = {}
+        gains = {}
+        while len(current_blocks_set) > 1:
             all_pairs = [list(tup) for tup in itertools.combinations(current_blocks_set, 2)]
             all_pairs_onehot = [self._encode_key(pair) for pair in all_pairs]
 
@@ -289,48 +290,43 @@ class SPP:
 
             best_pair = max(gain_map, key=gain_map.get)
 
-            # Remove invalidated elements from data structures
             to_remove_keys = self._find_keys_of_invalidated_elements(best_pair, current_blocks_set, gain_map)
             for block_index in to_remove_keys:
                 gain_map.pop(block_index, None)
 
-            # add to the set of current_blocks_set the new cluster
             current_blocks_set.add(tuple(best_pair))
 
-            # Compute the total gain for the current level
-            gain = 0
-            features_dict = {}
-            for cluster in current_blocks_set:
-                gain_delta, cluster_features = gain_function(self._encode_key([cluster]), self.feature_importance, self.f1_scores)
-                gain += gain_delta
-                features_dict[cluster] = cluster_features
-            compactness = ((self.n_classes - len(current_blocks_set))/(self.n_classes - 1))
-            total_gain = gain * compactness
-            gains_list.append(total_gain)
+            n_blocks = len(current_blocks_set)
+            if self.k is None or n_blocks <= self.k:
+                gain = 0
+                features_dict = {}
+                for cluster in current_blocks_set:
+                    gain_delta, cluster_features = gain_function(self._encode_key([cluster]), self.feature_importance, self.f1_scores)
+                    gain += gain_delta
+                    features_dict[cluster] = cluster_features
+                compactness = ((self.n_classes - n_blocks) / (self.n_classes - 1))
+                total_gain = gain * compactness
+                gains[n_blocks] = total_gain
+                dendogram[n_blocks] = (current_blocks_set.copy(), features_dict.copy())
+            else:
+                dendogram[n_blocks] = (current_blocks_set.copy(), {})
 
-            # Store the clusters for given level
-            dendogram[i] = (current_blocks_set.copy(), features_dict.copy())
+        self.precomputed_gains = gains
+        best_n = max(gains, key=gains.get)
 
-        self.precomputed_gains = gains_list
-        best_level = np.argmax(np.array(gains_list))
+        if self.k:
+            self.log.warning(f'Constrained to ≤{self.k} blocks, best gain is {gains[best_n]}')
 
-        # If specific level was set, override the best level
-        if self.fix_level:
-            best_level = self.n_classes - self.fix_level
-            best_total_gain = gains_list[best_level]
-            self.log.warning(f'The level was fixed at {self.fix_level}, the gain is {best_total_gain}')
-
-        # Process the dendogram to extract the solution
         blocks = []
         features_lists = []
-        n_blocks = range(len(dendogram[best_level][0]))
-        for block_index in n_blocks:
-            block_classes = list(dendogram[best_level][0])[block_index]
-            block_features = dendogram[best_level][1][block_classes]
+        n_blocks_range = range(len(dendogram[best_n][0]))
+        for block_index in n_blocks_range:
+            block_classes = list(dendogram[best_n][0])[block_index]
+            block_features = dendogram[best_n][1][block_classes]
             blocks.append(self._encode_key([block_classes]))
             features_lists.append(block_features)
 
-        dst_path = f'{self.use_case_name}_SPP_solution_LEVEL_{best_level}_{time.strftime("%Y%m%d_%H%M%S")}.csv'
+        dst_path = f'{self.use_case_name}_SPP_solution_NBLOCKS_{best_n}_{time.strftime("%Y%m%d_%H%M%S")}.csv'
         sol_df = self.cluster_sol_to_dataframe(blocks, features_lists)
         if print_console:
             print(tabulate(sol_df, headers = 'keys', tablefmt = 'psql'))
@@ -340,7 +336,7 @@ class SPP:
             sol_df.to_csv(dst_path, index=False)
 
         if show_plot_gain:
-            self.plot_gain(gains_list)
+            self.plot_gain(gains)
         return sol_df
 
     def _find_keys_of_invalidated_elements(self, best_pair, current_blocks_set, gain_map):
@@ -360,9 +356,11 @@ class SPP:
                         to_remove_keys.append(key)
         return to_remove_keys
 
-    def plot_gain(self, gains_list):
-        plt.plot(np.arange(self.n_classes, 1, -1), gains_list)
-        plt.xlabel('Level')
+    def plot_gain(self, gains):
+        n_blocks_list = sorted(gains.keys(), reverse=True)
+        gain_values = [gains[n] for n in n_blocks_list]
+        plt.plot(n_blocks_list, gain_values)
+        plt.xlabel('Number of blocks')
         plt.ylabel('Total Gain')
         plt.xlim(self.n_classes, 2)
         plt.xticks(np.arange(self.n_classes, 1, -1))
